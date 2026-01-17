f94a23d704d33345327b5ee4b058bdc12facc58e
Joshua Lina <lina.joshuacruz@gmail.com>
2026-01-18 02:08:27 +0800

    feat: Add menu browsing, cart management, and table selection features with associated UI, assets, and project configuration.

--- lib/features/table/presentation/pages/table_page.dart
+++ lib/features/table/presentation/pages/table_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../bloc/table_bloc.dart';
@@ -8,53 +9,194 @@ class TablePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<TableBloc, TableState>(
-      listener: (context, state) {
-        if (state is TableError) {
-          context.go('/404');
-        }
-      },
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Table Details')),
-        body: Center(
-          child: BlocBuilder<TableBloc, TableState>(
-            builder: (context, state) {
-              if (state is TableLoading) {
-                return const CircularProgressIndicator();
-              } else if (state is TableLoaded) {
-                final table = state.table;
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Table: ${table.description}',
-                      style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    Text('ID: ${table.tableId}'),
-                    if (table.uuid != null) Text('UUID: ${table.uuid}'),
-                    Text('Status: ${table.isActive ? "Active" : "Inactive"}'),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: BlocListener<TableBloc, TableState>(
+        listener: (context, state) {
+          if (state is TableError) {
+            context.go('/404');
+          }
+        },
+        child: Scaffold(
+          body: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/bg_image.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Center(
+                child: BlocBuilder<TableBloc, TableState>(
+                  builder: (context, state) {
+                    if (state is TableLoading) {
+                      return const CircularProgressIndicator();
+                    } else if (state is TableLoaded) {
+                      final table = state.table;
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipOval(
+                            child: Image.asset(
+                              'assets/images/logo.jpg',
+                              fit: BoxFit.contain,
+                              width: 150,
+                              height: 150,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                            decoration: BoxDecoration(
+                              color: Colors.black.withValues(alpha: 0.5),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Stack(
+                                  children: [
+                                    // Stroke Layer
+                                    Text(
+                                      "Welcome to Chickey's Inasal",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Marous',
+                                        foreground: Paint()
+                                          ..style = PaintingStyle.stroke
+                                          ..strokeWidth = 1
+                                          ..color = Colors.black, // Color of the stroke
+                                      ),
+                                    ),
+                                    // Solid Text Layer
+                                    Text(
+                                      "Welcome to Chickey's Inasal",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Marous',
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Stack(
+                                  children: [
+                                    // Stroke Layer
+                                    Text(
+                                      "Taste the best filipino chicken inasal",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w100,
+                                        foreground: Paint()
+                                          ..style = PaintingStyle.stroke
+                                          ..strokeWidth = 1
+                                          ..color = Colors.black,
+                                      ),
+                                    ),
+                                    // Solid Text Layer
+                                    Text(
+                                      "Taste the best filipino chicken inasal",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w100,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: 200,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xfff25125),
+                                foregroundColor: Colors.white,
+                                side: const BorderSide(color: Colors.white, width: 1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              onPressed: () => context.go('/table/${table.uuid}/menu'),
+                              child: const Text(
+                                'Start Ordering',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontFamily: 'SolemnSojourn',
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: 200,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xfff25125),
+                                foregroundColor: Colors.white,
+                                side: const BorderSide(color: Colors.white, width: 1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              onPressed: () => context.go('/table/${table.uuid}/order_summary'),
+                              child: const Text(
+                                'View Orders',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontFamily: 'SolemnSojourn',
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
 
-                    const SizedBox(height: 20),
-                    // Here we can eventually add the menu or order summary
-                    const Text('Menu and features will be loaded here.'),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () => context.go('/table/${table.uuid}/menu'),
-                      child: const Text('Ordering'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () => context.go('/table/${table.uuid}/order_summary'),
-                      child: const Text('View Orders'),
-                    ),
-                  ],
-                );
-              } else if (state is TableError) {
-                // This will be handled by the listener, but we can show a fallback here
-                return Text('Error: ${state.message}');
-              }
-              return const Text('Loading table info...');
-            },
+                          SizedBox(
+                            width: 200,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xfff25125),
+                                foregroundColor: Colors.white,
+                                side: const BorderSide(color: Colors.white, width: 1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                'Request bill',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontFamily: 'SolemnSojourn',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    } else if (state is TableError) {
+                      // This will be handled by the listener, but we can show a fallback here
+                      return Text('Error: ${state.message}');
+                    }
+                    return const Text('Loading table info...');
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
