bc46e3250f6402733c5d37bdfd4e901fb53ff95c
jeremdb <jeremiahdebien@gmail.com>
2026-01-24 01:07:23 +0800

    Add filtering on table

--- lib/features/table/data/datasources/table_supabase_datasource.dart
+++ lib/features/table/data/datasources/table_supabase_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../data/models/table_model.dart';
 
@@ -6,9 +7,11 @@ class TableSupabaseDataSource {
 
   TableSupabaseDataSource(this._client);
 
+  int get _branchId => int.tryParse(dotenv.env['BRANCH_ID'] ?? '') ?? 0;
+
   Future<TableModel> getTableByUuid(String uuid) async {
     try {
-      final response = await _client.from('tables').select().eq('table_uuid', uuid).single();
+      final response = await _client.from('tables').select().eq('table_uuid', uuid).eq('branch_id', _branchId).single();
 
       return TableModel.fromJson(response);
     } catch (e) {
